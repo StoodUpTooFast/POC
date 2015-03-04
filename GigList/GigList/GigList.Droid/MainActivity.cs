c2e4611ab8d54d9ec07389bf303c1d7ed4cbbaa2
StoodUpTooFast <pat.read@gmail.com>
2015-03-04 15:22:48 +0000

    Initial Project Creation

---
+++ GigList/GigList/GigList.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace GigList.Droid
+{
+    [Activity(Label = "GigList", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new GigList.App());
+        }
+    }
+}
+
---
+++ GigList/GigList/GigList.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GigList.WinPhone.Resources;
+
+namespace GigList.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
